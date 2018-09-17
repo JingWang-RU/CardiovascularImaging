cacd50ecc4a89115eef91a75014ba038d37ad652
jing wang <jing.julia.wang@gmail.com>
2018-09-17 09:20:33 -0400

    update

--- xmlpas.py
+++ xmlpas.py
@@ -1,35 +1,28 @@
 import lxml.etree as et
+import os,sys
 #author: Jing jiw2026@med.cornell.edu
 #function: read xml annotation from Medis software
 #time: Sept. 14th 2018
-f = open("attraction_points_vessel_wall.xml","r")
-#f = open("attraction_points_lumen.xml","r")
-xml = f.read()
-#wfile = open("lumen_pos.txt","w")
-wfile = open("vesselwall_pos.txt","w") # _* is the tag
-tree = et.fromstring(xml)
-for name in tree.xpath("//pos"):
-  wfile.write(name.text.strip()+'\n')
+# example:
+# python xmlpas.py lad attraction_points_vessel_wall pos  original_image_tags studyddate
+# python xmlpas.py ./lad/ attraction_points_lumen pos
+# python xmlpas.py ./lad/ pathline pos
+# python xmlpas.py ./lad/ transformation_matrices Item
+def readxmltotxt(path, xmlf, tag):
+  wpath = path + '/xml/'
+  if not os.path.exists(wpath):
+    os.makedirs(wpath)
+  f = open(path + xmlf + '.xml', "r")
+  #f = open("attraction_points_lumen.xml","r")
+  xml = f.read()
+  #wfile = open("lumen_pos.txt","w")
+  wfile = open(wpath + xmlf + '_' + tag + '.txt',"w") # _* is the tag
+  tree = et.fromstring(xml)
+  for name in tree.xpath('//' + tag):
+    wfile.write(name.text.strip()+'\n')
+    #print name.text.strip()
+  f.close()
+  wfile.close()
 
-  #print name.text.strip()
-f.close()
-wfile.close()
-'''
-
-tree = et.fromstring(xml)
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print ("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        print ("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print ("Encountered some data  :", data)
-
-# instantiate the parser and fed it some HTML
-parser = MyHTMLParser()
-parser.feed('<html><head><title>Test</title></head>'
-            '<body><h1>Parse me!</h1></body></html>')
-'''
+if __name__ == "__main__":
+     readxmltotxt(sys.argv[1], sys.argv[2], sys.argv[3])
