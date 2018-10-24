9ec77319b35e8c7de09887f26a1096d137ec0a0d
jing wang <jing.julia.wang@gmail.com>
2018-10-24 10:38:51 -0400

    update

--- Dicom_to_image.py
+++ Dicom_to_image.py
@@ -1,26 +1,45 @@
 # from tedivm in github pydicom
+#function: read dicom images e.g. 321165
 import numpy as np
 import png
 import pydicom
-# set by yourself
-path = "***.dcm"
-destination = './imag.jpeg'
+import os
+import glob
 
+def read_dicom_to_png(path, destination):
+    # path: a dicom file **.dcm ; destination: a png or jpeg file
+    ds = pydicom.dcmread(path)
 
-ds = pydicom.dcmread(path)
+    shape = ds.pixel_array.shape
+        
+        # Convert to float to avoid overflow or underflow losses.
+    image_2d = ds.pixel_array.astype(float)
 
-shape = ds.pixel_array.shape
+    # Rescaling grey scale between 0-255
+    image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+            
+    # Convert to uint
+    image_2d_scaled = np.uint8(image_2d_scaled)
+        
+    # Write the PNG file
+    with open(destination, 'wb') as png_file:
+       w = png.Writer(shape[1], shape[0], greyscale=True)
+       w.write(png_file, image_2d_scaled)
 
-# Convert to float to avoid overflow or underflow losses.
-image_2d = ds.pixel_array.astype(float)
+if __name__ == "__main__":
+    main_path = "./DICOM_KOREA/321165-2/MediaManager-201708181453569863/DICOM/ST00001/"
+    for i in range(10,16):
+     name = "SE000"+str(i)
+     dicom_path = main_path + name + '/'
+     #savepath = main_path + "imgJing/"
+     savepath = main_path + name + "imgJing/"
+     if not os.path.exists(savepath):
+        os.makedirs(savepath)
+     files_list = [f for f in os.listdir(dicom_path)]# if f.endswith(".dcm")]
+     #files_list = [f for f in os.listdir(dicom_path)]# if f.endswith(".dcm")]
 
-# Rescaling grey scale between 0-255
-image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+     for file in files_list:#glob.glob(dicom_path + "*.dcm"):
+        img_file = file + '.png'#
+        #img_file = file[:-4] + '.png'
+        read_dicom_to_png(dicom_path + file, savepath + img_file)
 
-# Convert to uint
-image_2d_scaled = np.uint8(image_2d_scaled)
-
-# Write the PNG file
-with open(destination, 'wb') as png_file:
-    w = png.Writer(shape[1], shape[0], greyscale=True)
-    w.write(png_file, image_2d_scaled)
